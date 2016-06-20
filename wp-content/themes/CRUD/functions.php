1d84bdb630351267d0a49cdee4e92a7ac0d73134
Fernando Menezes <wd.menezes@gmail.com>
2016-06-20 08:33:10 -0300

    initial commit

---
+++ wp-content/themes/CRUD/functions.php
@@ -0,0 +1,4 @@
+<?php
+register_nav_menus( array(
+	'menu-header' => 'menu header home Quatro Rodas'
+) );
---
+++ wp-content/themes/CRUD/static/js/ready.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+
+    $(".dropdown").hover(
+        function () {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true, true).slideDown("400");
+            $(this).toggleClass('open');
+        },
+        function () {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true, true).slideUp("400");
+            $(this).toggleClass('open');
+        }
+    );
+    $('#about_us_dropdown').slideDown();
+    $('#about_us_dropdown').toggleClass('open');
+
+
+});
